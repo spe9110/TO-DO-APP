a6fd94282fb50c4aaf20349564ee90daac3d135f
spe9110 <spencerm365@outlook.fr>
2026-01-02 17:09:11 +0100

    add the preview one

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/TO-DO-APP/' : '/', // GitHub Pages will serve the frontend at https://spe9110.github.io/TO-DO-APP/,
+  base: '/TO-DO-APP/', // GitHub Pages will serve the frontend at https://spe9110.github.io/TO-DO-APP/,
   build: {
     outDir: 'dist', // Where your build files will be placed
   },
